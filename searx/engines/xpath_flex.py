e8c6102352e37a889565ce809020c949e3ba8396
Dario Nuevo <dn@xbe.ch>
2022-01-11 15:15:12 +0100

    a bit of doc

--- searx/engines/xpath_flex.py
+++ searx/engines/xpath_flex.py
@@ -1,6 +1,47 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+
 """
- Yahoo (Web)
+ xPath Flex Engine (xpath_flex)
+
+ Aims to be a more flexible engine that covers xpath use cases for all categories
+ (as it allows to override the result template).
+ The engine configuration expects you to specify a 'field_definition' dictionary with the following properties:
+ * field_name: the template field that should be set by this entry.
+ * xpath: a xpath expression to apply
+ * (optional) single_element: if True, will execute the xpath expression to expect a single node, not a list
+ * (optional) extract: a string specifying an extraction/conversion of the value, the following is supported:
+   "url": extracts an url using base search url
+   "boolean": will convert the value passed to the template to True if the xpath matches any node
+   "boolean_negate": will convert the value passed to the template to True if the xpath does NOT match a node
+   by default, text will be extracted from the found node(s)
+
+ an example would look like this (for an example shopping site with paging):
+
+   - name : xpath-flex-example
+    engine : xpath_flex
+    paging : True
+    search_url : https://myl-shopping.site/search?q={query}&p={pageno}
+    template : products.html
+    results_xpath : //div[@class="listing--container"]/div[@class="listing"]/div[contains(@class,"product--box")]
+    field_definition:
+      - field_name: url
+        xpath: (.//a[contains(@class,"product--image")])/@href
+        extract: url
+      - field_name: title
+        xpath: (.//a[contains(@class,"product--image")])/@title
+      - field_name: content
+        xpath: .//div[@class="product--description"]/text()
+      - field_name: price
+        xpath: .//div[@class="product--price"]/span/text()
+      - field_name: thumbnail
+        xpath: substring-before( (.//span[@class="image--media"]/img)/@srcset, ", ")
+        extract: url
+        single_element: True
+      - field_name: has_stock
+        xpath: .//a[contains(@class,"buynow")][not(contains(@class,"is--disabled"))]
+        extract: boolean
+        single_element: True
+
 """
 
 from lxml import html
